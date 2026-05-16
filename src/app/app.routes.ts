9c3f836554d5755ce5ea05f742b8812ff70db9fe
R1CKYS3NS31 <derricksonndambuki@gmail.com>
2026-05-16 15:44:12 +0300

    feat: initialize project structure, add Angular Material theme, and configure Firebase environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    // component: Layout,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./ui/features/home/home').then(m => m.Home)
+      },
+      //  {
+      //   path: 'account',
+      //   loadComponent: () => import('./features/account/account').then(m => m.Account)
+      // },
+      // {
+      //   path: 'profile/:id',
+      //   loadComponent: () => import('./features/profile/profile').then(m => m.Profile)
+      // },
+      // {
+      //   path: 'signup',
+      //   loadComponent: () => import('./features/auth/signup/signup').then(m => m.Signup)
+      // },
+      // {
+      //   path: 'signin',
+      //   loadComponent: () => import('./features/auth/signin/signin').then(m => m.Signin)
+      // },
+      // {
+      //   path: '**',
+      //   loadComponent: () => import('./features/not-found/not-found').then(m => m.NotFound)
+      // }
+    ]
+  }
+];
