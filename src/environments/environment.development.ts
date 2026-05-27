591564bd150de353680377fe6e46c5bfb4b2a02b
R1CKYS3NS31 <derricksonndambuki@gmail.com>
2026-05-27 06:37:53 +0300

    uncomment environment files in .gitignore for proper tracking

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-   
+    apiKey: "AIzaSyCPupo736A6u2G7PguHJnZr_IvZU4P7Odk",
+    authDomain: "pov-firebase.firebaseapp.com",
+    projectId: "pov-firebase",
+    storageBucket: "pov-firebase.firebasestorage.app",
+    messagingSenderId: "105213180884",
+    appId: "1:105213180884:web:bccb1f72bd2b05e4997441",
+    measurementId: "G-0639D77MJV"
   }
 };
